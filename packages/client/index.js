9100a7110e2cc53f6df9bdad4871c87457c4e79d
Sonny Piers <sonny@crypho.com>
2017-10-06 17:20:02 +0200

    react native hack

--- packages/client/index.js
+++ packages/client/index.js
@@ -1,5 +1,11 @@
 'use strict'
 
+try {
+  const p = require('react-native').Platform
+  global.Buffer = require('buffer').Buffer
+  global.process = require('process')
+} catch (e) {}
+
 const Client = require('./lib/Client')
 const {xml, jid} = require('@xmpp/client-core')
 
